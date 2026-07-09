8abb91d40bf9caef94e3b81d8583b790dbbdf3ac
patmcgtx <57506933+patmcgtx@users.noreply.github.com>
2026-07-08 19:42:05 -0500

    Initial main view

---
+++ ConcurrencyDashboard/ConcurrencyDashboard/Views/MainView.swift
@@ -0,0 +1,17 @@
+//
+//  MainView.swift
+//  ConcurrencyDashboard
+//
+//  Created by Patrick McGonigle on 7/8/26.
+//
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("hello")
+    }
+}
+
+#Preview {
+    MainView()
+}
